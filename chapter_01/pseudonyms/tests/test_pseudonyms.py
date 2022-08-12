71ca3fa6f66e24b0f1707e3b0cb1a06d6024d68d
ph_santosjr <paulo.henrique@databrain.com.br>
2022-08-12 15:37:54 -0300

    create tests

--- chapter_01/pseudonyms/tests/test_pseudonyms.py
+++ chapter_01/pseudonyms/tests/test_pseudonyms.py
@@ -3,9 +3,10 @@ import pytest
 from chapter_01.pseudonyms.main import Pseudonymns
 
 
-class PseudonymnsTest:
+class TestPseudonymns:
     def test_total_number_is_number(self):
         p = Pseudonymns()
         assert isinstance(p.total_names, int)
+
 
 
